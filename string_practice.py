3619a19c020f87f25051d760378168b77ebd3d94
amit sharma <amitsharmajif@gmail.com>
2026-06-22 12:37:57 +0530

    first commit

---
+++ string_practice.py
@@ -0,0 +1,34 @@
+# # wap to remove last word from string 
+# str1 = "This is a python code in VS"
+# str1 = str1[:-1]
+# for i in range(len(str1)-1, -1, -1):
+#     print(str1)
+
+# #wap to remove all the vowels from string
+# str2 = "This is a python code in VS"
+# vowels = "aeiouAEIOU"
+# str2 = str2.translate(str.maketrans('', '', vowels))
+# print(str2) 
+
+# #waf to count white spaces in a string
+# str3 = "This is a python code in VS"
+# count = str3.count(" ")
+# print(count)
+# # waf to extract only numbers from str1
+# str1="This is python code 123 in VS version address noida-11096"
+# numbers = ''.join(filter(str.isdigit, str1))
+# print(numbers)
+# # waf to remove all spaces from str1
+# str1 = "This is a python code in VS version address noida-11096"
+# str1 = str1.strip()
+# print(str1)
+# # waf to print all special symbols 
+# str1="This is python code@123in VS vesion addresss noida-11096"
+# for str1, remove[] 
+# #waf to reverse allwords of strin.
+# str1="This is python code@123 in VS vesion addresss noida-11096"
+# str1=str1[::-1]
+# print(str1)
+
+
+print("hello")
